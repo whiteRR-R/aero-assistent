7846dcf477e076d75c447447f2ed27ffc0bd0b16
whiteRR <whiterrs.zzz@gmail.com>
2026-05-17 09:10:00 +0500

    fix(errors): базовые исключения

---
+++ aero-backend/src/main/java/com/aero/exception/AeroException.java
@@ -0,0 +1,4 @@
+package com.aero.exception;
+public class AeroException extends RuntimeException {
+    public AeroException(String message) { super(message); }
+}
---
+++ aero-backend/src/main/java/com/aero/exception/BadRequestException.java
@@ -0,0 +1,4 @@
+package com.aero.exception;
+public class BadRequestException extends AeroException {
+    public BadRequestException(String message) { super(message); }
+}
